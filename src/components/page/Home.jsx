b71eccb7818aea56f74d4affbec4f4dbc0afb7ab
parksangseok <>
2024-12-05 16:20:44 +0900

    conflict 해결 중 삭제 파일 복원 후 재커밋

---
+++ src/components/page/Home.jsx
@@ -0,0 +1,13 @@
+import Main from "./Main";
+import Footer from "../section/Footer";
+import Header from "../section/Header";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
