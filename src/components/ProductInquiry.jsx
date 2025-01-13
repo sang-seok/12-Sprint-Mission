587d10eb73527fd2c52ce0f372d15d9a6324ffd5
parksangseok <>
2025-01-13 13:48:35 +0900

    build: textarea type 삭제

--- src/components/ProductInquiry.jsx
+++ src/components/ProductInquiry.jsx
@@ -1,4 +1,3 @@
-
 export default function ProductInquiry() {
 
   return (
@@ -13,12 +12,12 @@ export default function ProductInquiry() {
         </label>
         <textarea
           id="inquiry"
-          type="text"
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다.
 " />
 
         <div className="btnBox">
-          <button class="addBtn" >
+          <button
+            class="addBtn">
             등록
           </button>
         </div>
